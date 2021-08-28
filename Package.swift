3fa4dc7db95833a2adf850df44a7983c7ed8a725
lishuailibertine <lishuai19@yeah.net>
2021-08-28 19:14:00 +0800

    0.1.2

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "fealessUtil",
+    name: "FearlessUtils",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "fealessUtil",
-            targets: ["fealessUtil"]),
+            name: "FearlessUtils",
+            targets: ["FearlessUtils"]),
     ],
     dependencies: [
         .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "fealessUtil",
+            name: "FearlessUtils",
             dependencies: ["BigInt","TweetNacl","xxHash-Swift","SS58Factory","scrypt"]),
         .testTarget(
             name: "fealessUtilTests",
-            dependencies: ["fealessUtil"]),
+            dependencies: ["FearlessUtils"]),
     ]
 )
