6ba43365d7e906b4568c1e1a160708456b12c8e4
lishuailibertine <libertine9458@gmail.com>
2024-11-05 17:29:00 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -12,15 +12,15 @@ let package = Package(
             targets: ["FearlessUtils"]),
     ],
     dependencies: [ 
-        .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
-        .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap", from: "1.0.3"),
+        .package(name: "BigInt", url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+        .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap", from: "1.0.5"),
         .package(name: "SubstrateKeychain", url: "https://github.com/lishuailibertine/SubstrateKaychain", from: "0.1.5"),
         .package(name: "SS58Factory", url: "https://github.com/lishuailibertine/SS58Factory", from: "1.0.0"),
         .package(name: "RobinHood", url: "https://github.com/lishuailibertine/robinhood-ios", from: "3.0.0"),
         .package(name: "SwiftyBeaver", url: "https://github.com/SwiftyBeaver/SwiftyBeaver", from: "1.9.5"),
         .package(name: "Starscream", url: "https://github.com/daltoniam/Starscream", from: "4.0.8"),
         .package(url: "https://github.com/Flight-School/AnyCodable.git", .exact("0.6.1")),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
